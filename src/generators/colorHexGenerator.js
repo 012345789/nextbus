307886a388f46b9c93a8f37469c948e66fe9bb2b
012345789 <holyduper@gmail.com>
2017-07-25 11:49:23 -0700

    Minor color change

--- src/generators/colorHexGenerator.js
+++ src/generators/colorHexGenerator.js
@@ -6,7 +6,7 @@ export default function generateColorHex(str) {
 
 // https://stackoverflow.com/a/3426956
 function hashCode(str) {
-  str = str + "randomString"; // for wider spectrum of hashing
+  str = str + "randomString" + str; // for wider spectrum of hashing
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
